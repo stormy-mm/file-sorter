689615042d38fdf1a1c8729b5148165ef44f24a9
михаил <maksutenkomihail@gmail.com>
2026-01-09 22:51:04 +0300

    Написаны мт и ядро программы. тесты проходят

---
+++ DOMENS/file_sorter.py
@@ -0,0 +1,65 @@
+import os
+
+
+def adding_path(*args: tuple) -> str:
+    """Возвращает склеенный путь"""
+    return os.path.join(*args)
+
+class FileSorter:
+    """Класс для управления сортировщиком файлов"""
+
+    def __init__(self):
+        """Инициализация словаря с названиями директорий и расширениями"""
+        self._DICT_WITH_EXTENSION = {
+            "Images": [".jpg", ".jpeg", ".png", ".svg"],
+            "Documents": [".doc", ".docx", ".txt", ".pdf", ".xlsx", ".pptx"],
+            "Archives": [".zip", ".gz", ".tar"],
+            "Audio": [".mp3", ".ogg", ".wav", ".amr"],
+            "Video": [".avi", ".mp4", ".mov", ".mkv"],
+            "Other": []
+        }
+
+    def get_extension(self) -> dict:
+        """Возвращает словарь папок и расширений"""
+        return self._DICT_WITH_EXTENSION
+
+    def get_folder_user(self, folder_name: str) -> list:
+        """Возвращает директорию пользователя"""
+        return os.listdir(folder_name)
+
+    def create_folder_in_user(self, folder_name: str) -> bool:
+        """Создание папки в директории пользователя"""
+        for folder in self.get_extension().keys():
+            os.makedirs(adding_path(folder_name, folder), exist_ok=True)
+        return True
+
+    def checker_files_in_folder_user(self, folder_name: str) -> bool:
+        """Проверяет отсутствие файлов в директории"""
+        for file in self.get_folder_user(folder_name):
+            if os.path.isfile(adding_path(folder_name, file)):
+                return False
+        return True
+
+    def _get_target_folder(self, extension: str) -> str:
+        for folder, extensions in self.get_extension().items():
+            if extension in extensions:
+                return folder
+        return "Other"
+
+    def sort_files(self, folder_user: str) -> bool:
+        for file in self.get_folder_user(folder_user):
+            src = adding_path(folder_user, file)
+
+            if not os.path.isfile(src):
+                continue
+
+            extension = os.path.splitext(file)[1]
+            target_folder = self._get_target_folder(extension)
+            dst = adding_path(folder_user, target_folder, file)
+
+            try:
+                os.rename(src, dst)
+            except FileExistsError:
+                pass
+
+        return True
--- TESTS/mt_sorter.py
+++ TESTS/mt_sorter.py
@@ -1,41 +1,79 @@
+import shutil
 import unittest
 import os
-# from DOMENS.file_sorter import FileSorter
+from DOMENS.file_sorter import FileSorter, adding_path
 
-DIR = "MESS"
-FILES = ("notes.txt", "report.docx", "vacation.jpg", "archive.zip", "track1.mp3", "my_script.py")
-FOLDERS = (os.path.join("old_photos", "family.png"), os.path.join("new_photos", "python.png"))
 
-# Создание директории с файлами
-os.chdir("..")  # выход из TESTS
-print(os.getcwd())
-# os.makedirs(DIR, exist_ok=True)
-#
-# # Создание файлов
-# for file in FILES:
-#     open(os.path.join(DIR, file), "w")
-#
-# # Создание подпапок
-# for folder in FOLDERS:
-#     os.makedirs(os.path.join(DIR, folder), exist_ok=True)
+PATH = fr"{adding_path(os.getcwd(), "MESS")}"
+FILES = ("notes.txt", "report.docx", "vacation.jpg", "archive.zip", "track1.mp3", "my_script.py", "what_the_fuck.wtf")
+FOLDERS = (adding_path("old_photos", "family.png"),
+           adding_path("new_photos", "special_photos", "python.jpeg"),
+           adding_path("new_photos", "hidden_photos", "python313.jpg"))
 
-#
-# class TestFileSorter(unittest.TestCase):
-#     """Тест реализации ядра программы"""
-#
-#     def setUp(self):
-#         """
-#         Подгрузка файлов
-#
-#         В учебных целях я буду создавать в той же директории, что и вся программа
-#         """
-#
-#
-#
-#         # Работа завершена. Возвращаемся в TESTS
-#         # os.chdir("TESTS")
-#
-#     # def tearDown(self):
-#     #     """Удаление файлов"""
-#     #     for file in FILES:
-#     #         os.remove(os.path.join(DIR, file))
+
+class TestFileSorter(unittest.TestCase):
+    """Тест реализации ядра программы"""
+
+    def setUp(self):
+        """
+        Подгрузка файлов
+
+        В учебных целях я буду создавать в той же директории, что и вся программа
+        """
+        os.makedirs(PATH, exist_ok=True)
+
+        # Создание файлов
+        for file in FILES:
+            with open(adding_path(PATH, file), "a"):
+                pass
+
+        # Создание подпапок
+        for folder in FOLDERS:
+            os.makedirs(adding_path(PATH, folder), exist_ok=True)
+
+        print("Тест начался. Файлы созданы")
+
+    def tearDown(self):
+        """Удаление файлов и папок"""
+        shutil.rmtree(PATH)
+        print("Тест завершился. Файлы подчищены")
+
+    def test_dict_with_extension(self):
+        """Тест: наличие словаря с расширениями"""
+        self.assertTrue(FileSorter().get_extension())
+
+    def test_can_get_folder_user(self):
+        """Тест: можно получить директорию пользователя"""
+        self.assertTrue(FileSorter().get_folder_user(PATH))
+
+    def test_ERROR_if_user_input_invalid_path(self):
+        """Тест: выбрасывается исключение, если вводится несуществующий путь"""
+        with self.assertRaises(FileNotFoundError):
+            FileSorter().get_folder_user(adding_path(os.path.split(PATH)[0], "XZ"))
+
+    def test_can_create_folder(self):
+        """Тест: папка создаётся без проблем"""
+        self.assertTrue(FileSorter().create_folder_in_user(PATH))
+
+    def test_file_with_extension_in_user_folder(self):
+        """Тест: проверяет наличие файлов в директории пользователя ПЕРЕД сортировкой"""
+        self.assertFalse(FileSorter().checker_files_in_folder_user(PATH))
+
+    def test_sort_is_True(self):
+        """Тест: сортировка прошла успешно"""
+        FileSorter().create_folder_in_user(PATH)
+        self.assertTrue(FileSorter().sort_files(PATH))
+
+    def test_file_with_extension_in_the_correct_folder(self):
+        """Тест: файл ПОСЛЕ сортировки находится в правильной папке"""
+        FileSorter().create_folder_in_user(PATH)
+        FileSorter().sort_files(PATH)
+        self.assertIn(FILES[0], os.listdir(adding_path(PATH, "Documents"))) # notes.txt
+        self.assertIn(FILES[4], os.listdir(adding_path(PATH, "Audio"))) # track1.mp3
+
+    def test_file_with_extension_in_not_user_folder(self):
+        """Тест: проверяет наличие файлов в директории пользователя ПОСЛЕ сортировки"""
+        fs = FileSorter()
+        fs.create_folder_in_user(PATH)
+        fs.sort_files(PATH)
+        self.assertTrue(fs.checker_files_in_folder_user(PATH))

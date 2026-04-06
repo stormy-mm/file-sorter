fa06232a91a5226a4ce507844b28f9a4a75b34db
stormy_mm <maksutenkomihail@gmail.com>
2026-04-06 23:12:23 +0300

    Тесты переписаны с переходом с os на pathlib

--- tests/unit/test_sorting.py
+++ tests/unit/test_sorting.py
@@ -1,15 +1,11 @@
-import os
+from pathlib import Path
+from typing import Iterator
 
 from pytest import fixture, raises
 
 from my_app.common.exceptions import InvalidPathError
-from my_app.core.file_sorter import FileSorter, adding_path
-
-
-FILES = ("notes.txt", "report.docx", "vacation.jpg", "archive.zip", "track1.mp3", "my_script.py", "what_the_fuck.wtf")
-FOLDERS = (adding_path("old_photos", "family.png"),
-           adding_path("new_photos", "special_photos", "python.jpeg"),
-           adding_path("new_photos", "hidden_photos", "python313.jpg"))
+from my_app.core.file_sorter import FileSorter
+from my_app.common.files_folders import FOLDERS, FILES
 
 
 class TestFileSorter:
@@ -26,40 +22,51 @@ class TestFileSorter:
         self.path = tmp_path
         # Создание файлов
         for file in FILES:
-            with open(adding_path(self.path, file), "a"):
+            with open(self.path / file, "a"):
                 pass
 
         # Создание подпапок
         for folder in FOLDERS:
-            os.makedirs(adding_path(self.path, folder), exist_ok=True)
+            Path(self.path / folder).mkdir(exist_ok=True, parents=True)
+
+    def test_valid_path_is_correct(self, setup):
+        """Тест: введённый путь корректен: исключение не поднимается"""
+        self.filesorter.path_check(self.path)
+
+    def test_invalid_path_is_not_correct(self, setup):
+        """Тест: введённый путь с расширением на конце некорректен"""
+        with raises(InvalidPathError):
+            self.filesorter.path_check(self.path / "test.txt")
 
     def test_can_get_folder_user(self, setup):
-        """Тест: можно получить директорию пользователя"""
-        assert self.filesorter.get_folder(self.path)
+        """Тест: можно получить содержимое директории пользователя"""
+        assert isinstance(self.filesorter.get_folder(self.path), Iterator)
 
     def test_error_if_user_input_invalid_path(self, setup):
         """Тест: выбрасывается исключение, если вводится несуществующий путь"""
         with raises(FileNotFoundError):
-            self.filesorter.get_folder(adding_path(os.path.split(self.path)[0], "XZ"))
+            self.filesorter.get_folder(self.path / "test")
 
     def test_can_create_folder(self, setup):
         """Тест: папка создаётся без проблем"""
-        assert self.filesorter.create_folder(self.path)
+        self.filesorter.create_folders(self.path)
+        assert (self.path / "Documents").exists()
+        assert (self.path / "Other").exists()
 
     def test_cannot_create_folder_if_user_input_file(self, setup):
         """Тест: ошибка, если пользователь введёт путь к файлу"""
         with raises(InvalidPathError):
-            self.filesorter.create_folder(self.path / "test.txt")
+            self.filesorter.create_folders(self.path / "test.txt")
 
     def test_file_with_extension_in_the_correct_folder(self, setup):
         """Тест: файл после сортировки находится в правильной папке"""
-        self.filesorter.create_folder(self.path)
+        self.filesorter.create_folders(self.path)
         self.filesorter.sort_files(self.path)
-        assert FILES[0] in os.listdir(adding_path(self.path, "Documents")) # notes.txt
-        assert FILES[4] in os.listdir(adding_path(self.path, "Audio")) # track1.mp3
+        assert (self.path / r"Documents\notes.txt").exists()
+        assert (self.path / r"Audio\track1.mp3").exists()
 
     def test_file_with_extension_in_not_user_folder(self, setup):
         """Тест: в директории пользователя нет файлов после сортировки"""
-        self.filesorter.create_folder(self.path)
+        self.filesorter.create_folders(self.path)
         self.filesorter.sort_files(self.path)
         assert self.filesorter.is_root_folder_clean(self.path)

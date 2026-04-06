57bf95094c8e81bbc16a7d05a3547ab6cc9cb90f
stormy_mm <maksutenkomihail@gmail.com>
2026-04-06 23:12:45 +0300

    убрана функция adding_path

---
+++ src/my_app/common/files_folders.py
@@ -0,0 +1,8 @@
+"""Файл с загрузочными файлами и директориями"""
+
+FILES = ("notes.txt", "report.docx", "vacation.jpg", "archive.zip", "track1.mp3", "my_script.py", "what_the_fuck.wtf")
+FOLDERS = (
+    r"old_photos\family.png",
+    r"new_photos\special_photos\python.jpeg",
+    r"new_photos\hidden_photos\python313.jpg",
+)

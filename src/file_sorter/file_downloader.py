f811791d12d622e514b66b931de5ab9ab48f5c29
stormy_mm <maksutenkomihail@gmail.com>
2026-04-07 23:18:57 +0300

    небольшой рефакторинг

--- src/file_sorter/file_downloader.py
+++ src/file_sorter/file_downloader.py
@@ -4,15 +4,15 @@ from file_sorter.common.files_folders import FOLDERS, FILES
 
 PATH = Path().cwd() / "../../mess"
 
-Path(PATH).mkdir(exist_ok=True)
+PATH.mkdir(exist_ok=True)
 
 # Создание файлов
 for file in FILES:
     with open(PATH / file, "a"):
-        ...
+        pass
 
 # Создание подпапок
 for folder in FOLDERS:
-    Path(PATH / folder).mkdir(exist_ok=True, parents=True)
+    (PATH / folder).mkdir(exist_ok=True, parents=True)
 
 print("Файлы созданы")

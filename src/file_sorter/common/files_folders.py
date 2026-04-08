7d4adb8114b14a6cb1293292f54d397f6aae33c4
stormy_mm <maksutenkomihail@gmail.com>
2026-04-08 13:40:18 +0300

    исправлены пути для linux

--- src/file_sorter/common/files_folders.py
+++ src/file_sorter/common/files_folders.py
@@ -1,7 +1,15 @@
 """Файл с загрузочными файлами и директориями"""
 from pathlib import Path
 
-FILES = ("notes.txt", "report.docx", "vacation.jpg", "archive.zip", "track1.mp3", "my_script.py", "what_the_fuck.wtf")
+FILES = (
+    "notes.txt",
+    "report.docx",
+    "vacation.jpg",
+    "archive.zip",
+    "track1.mp3",
+    "my_script.py",
+    "what_the_fuck.wtf",
+)
 FOLDERS = (
     Path("old_photos", "family.png"),
     Path("new_photos", "special_photos", "python.jpeg"),
